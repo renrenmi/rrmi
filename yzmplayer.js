b9c59813b6c536faea08e20f142e45ba082a61d2
renrenmi <78365481+renrenmi@users.noreply.github.com>
2021-11-26 11:13:58 +0800

    Update yzmplayer.js

--- yzmplayer.js
+++ yzmplayer.js
@@ -2625,4 +2625,3 @@ var yzmck = {
 	}
 };
 
-document.write(unescape("%3Cspan id='cnzz_stat_icon_1280614520'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1280614520' type='text/javascript'%3E%3C/script%3E"));
